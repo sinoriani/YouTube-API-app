a5b838b4aa697ce901894d3a49f2ac9de29d040a
yassine fixe win10 <yassineriani@gmail.com>
2021-04-02 14:22:07 +0100

    added comments route in client and server. also can retrieve a video's comments now

--- server/routes/youtubeAuth.js
+++ server/routes/youtubeAuth.js
@@ -9,7 +9,7 @@ const oauthRouter = express.Router();
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/youtube-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly','https://www.googleapis.com/auth/userinfo.profile'];
+var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly','https://www.googleapis.com/auth/userinfo.profile','https://www.googleapis.com/auth/youtube.force-ssl'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
   process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
