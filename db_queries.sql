c39e1970455627678df3b2f0d89bddde70ac7837
Yassine Ubuntu Fixe <yassineriani@gmail.com>
2021-05-09 14:00:48 +0100

    added recommended vids to home page

---
+++ db_queries.sql
@@ -0,0 +1,17 @@
+
+create table watch_history(
+    id integer primary key auto_increment,
+    video_id char(11),
+    user_id char(25) references user(id),
+    timestamp_ timestamp default current_timestamp
+);
+
+create table user(
+    id varchar(25) primary key, 
+    name varchar(150), 
+    given_name varchar(150), 
+    family_name varchar(150), 
+    picture varchar(400), 
+    locale varchar(10), 
+    points integer default 0
+);
